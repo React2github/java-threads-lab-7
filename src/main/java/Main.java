3e0bca9efb51f751d74128a27a17cedceb65ac96
Abdella <abdella.shukur2@gmail.com>
2022-07-28 07:08:03 -0400

    lab 7 working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class Main {
-    public static Callable<Integer> getCallableInteger() {
+    public static Callable<Integer> getCallableInteger(Scanner scanner)  {
         // your code here
+return (new Callable<Integer>() {
+    @Override
+    public Integer call() throws Exception {
+        return scanner.nextInt();
+    }
+});
+    }
+
+    public static void main(String[] args) throws ExecutionException,InterruptedException{
+
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        int inputValue = executor.submit(getCallableInteger(new Scanner(System.in))).get();
+        System.out.println(inputValue);
+        executor.shutdown();
     }
 }
